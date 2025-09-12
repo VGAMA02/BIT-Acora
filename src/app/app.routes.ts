0c3bb616a4109848db403abb00dd6d92de0a48ea
Gama <gamagama90@hotmail.com>
2025-09-12 13:06:47 -0600

    estilos calendario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CalendarComponent } from './pages/calendar/calendar.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+
 export const routes: Routes = [    {
         path:'',
         redirectTo:'home',
@@ -13,6 +16,14 @@ export const routes: Routes = [    {
     {
         path:'calendar',
         component: CalendarComponent
+    },
+    {
+        path:'register',
+        component: RegisterComponent
+    },
+    {
+        path:'login',
+        component: LoginComponent
     },
 
 
